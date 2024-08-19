cda5a437c46ea89606286bbe16093598782a1d28
KatherinaKulinich <kulinichcatherina@gmail.com>
2024-08-19 11:11:43 +0300

    added styles to UserDetails component

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -13,39 +13,49 @@ const UserDetails: React.FC<UserDetailsProps> = ({ user }) => {
         created_at,
         bio,
         location,
-        name,
+        name
     } = user;
 
     return (
-        <div>
-            <img src={avatar_url} />
-            <div>
-                <div>
-                    <h2>{login}</h2>
-                    <p>{name}</p>
-                    <p>{bio}</p>
+        <div className='flex flex-col gap-12 p-4 items-center'>
+            <img
+                className='rounded-full w-72 h-72'
+                src={avatar_url}
+            />
+            <div className='flex flex-col gap-6'>
+                <div className='flex flex-col gap-3'>
+                    <div className='flex flex-col items-center'>
+                        <h2 className='font-bold text-xl'>{login}</h2>
+                        <p className='font-light text-md text-slate-600'>
+                            {name}
+                        </p>
+                    </div>
+                    <p className='text-sm'>{bio}</p>
                 </div>
-                <div>
+                <div className='font-light text-sm text-slate-600'>
                     <p>{location}</p>
                     <p>Profile had been created at: {created_at}</p>
                 </div>
-                <div>
-                    <div>
+                <div className='flex items-center justify-between'>
+                    <div className='flex items-center gap-2 text-md text-slate-600 font-semibold'>
                         <p>Followers:</p>
                         <p>{followers}</p>
                     </div>
-                    <div>
+                    <div className='flex items-center gap-2 text-md text-slate-600 font-semibold'>
                         <p>Following:</p>
                         <p>{following}</p>
                     </div>
                 </div>
             </div>
-            <a
-                href={`https://github.com/${login}`}
-                target='_blank'
-            >
-                Go to GitHub profile
-            </a>
+            <div className='flex items-center justify-center'>
+                <a
+                    href={`https://github.com/${login}`}
+                    target='_blank'
+                    className='px-4 py-2 border rounded-lg uppercase bg-sky-500 text-sky-50 font-bold'
+                >
+                    Go to GitHub profile
+                </a>
+            </div>
         </div>
     );
 };
